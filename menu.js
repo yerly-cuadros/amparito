625c28b3ad7284d87cc1ec96daf5592495e50f46
yerly <yerlycuadros@gmail.com>
2020-03-09 07:35:09 -0500

    amparo

---
+++ menu.js
@@ -0,0 +1,6 @@
+var btMenu = document.getElementById('btmenu');
+var menu = document.getElementById('menu');
+btMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
